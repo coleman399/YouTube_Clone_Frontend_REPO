0f62522a9ce383f6968e5a4caee2e09d8c628bf0
Dillon Coleman <coleman399@gmail.com>
2021-11-30 16:15:33 -0700

    added some functionality to comments

--- src/components/CommentSection/Comment.jsx
+++ src/components/CommentSection/Comment.jsx
@@ -1,15 +1,19 @@
 import React, {useState} from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Collapse from 'react-bootstrap/Collapse';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 const Comment = (props) => {
     const [likes, setLikes] = useState(props.commentLikes)
     const [dislikes, setDislikes] = useState(props.commentDislikes)
     const [toggle1, setToggle1] = useState('')
     const [toggle2, setToggle2] = useState('')
-    const [reply,SetReply] = useState('')
-    const [replyLikes,SetReplyLikes] = useState(0)
-    const [replyDislikes,SetReplyDislikes] = useState(0)
+    const [open, setOpen] = useState(false);
+    const [reply, SetReply] = useState('')
+    const [replyLikes, SetReplyLikes] = useState(0)
+    const [replyDislikes, SetReplyDislikes] = useState(0)
 
     const handleClick = async(parentId)=>{
         reply={
@@ -33,13 +37,26 @@ const Comment = (props) => {
                                 <div className="d-grid gap-2">
                                     <Button variant="success" onClick={() => {setLikes((likes + 1)); setToggle1('disabled');}} disabled={toggle1}>Like {likes}</Button>
                                     <Button variant="danger" onClick={() => {setDislikes((dislikes + 1)); setToggle2('disabled');}} disabled={toggle2}>Dislike {dislikes}</Button>
-                                    <Button onClick={()=>handleClick(props.parentId)}variant="primary">Reply</Button>
+                                    <Button variant="primary" onClick={() => {setOpen(!open); handleClick(props.parentId)}}  aria-controls="reply" aria-expanded={open} >Reply</Button>
                                 </div>
                             </div>
                         </div>  
                     </Card.Body>
                 </Card>
-            </div>        
+            </div>
+            <Collapse in={open} alignment="center">
+                <div>
+                <Form>
+                    <FloatingLabel controlId="reply" label="Reply...">
+                        <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}
+                        />
+                    </FloatingLabel>
+                </Form>
+                </div>
+            </Collapse>        
         </div>
     )
 }
